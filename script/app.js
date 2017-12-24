b8c90e292c9552dea40badd837bdeded87ecf93b
yurkagon <owngameplay@gmail.com>
2017-12-25 00:14:10 +0200

    Some changes in collision function, FINALgit add .!

--- script/app.js
+++ script/app.js
@@ -39,23 +39,23 @@ class Player{
 		var result = false;
 
 		obj.each(function(){
-			if( isCollised($(this),playerQuerry) ){
+			if( isCollised($(this))){
 				result = true;
 				return false; //breaking
 			}
 		});
 
-		function isCollised($div1, $div2) {
-			var x1 = $div1.offset().left;
-			var y1 = $div1.offset().top;
-			var h1 = $div1.outerHeight(true);
-			var w1 = $div1.outerWidth(true);
+		function isCollised($div) {
+			var x1 = $div.offset().left;
+			var y1 = $div.offset().top;
+			var h1 = $div.outerHeight(true);
+			var w1 = $div.outerWidth(true);
 			var b1 = y1 + h1;
 			var r1 = x1 + w1;
-			var x2 = $div2.offset().left;
-			var y2 = $div2.offset().top;
-			var h2 = $div2.outerHeight(true);
-			var w2 = $div2.outerWidth(true);
+			var x2 = playerQuerry.offset().left;
+			var y2 = playerQuerry.offset().top;
+			var h2 = playerQuerry.outerHeight(true);
+			var w2 = playerQuerry.outerWidth(true);
 			var b2 = y2 + h2;
 			var r2 = x2 + w2;
 
